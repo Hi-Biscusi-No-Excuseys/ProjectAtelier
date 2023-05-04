fe1ad35b0f0923df8ad1c76da30bf2dd50ba1f4a
Philip <pwang0407@gmail.com>
2023-05-04 12:12:21 -0700

    Overall Structure Completed

---
+++ client/src/components/QuestionsAnswers/AddQuestion.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { useState } from 'react';
+//
+export default function AddQuestion() {
+  return (
+    <div>
+      <button type="submit">Add a Question +</button>
+    </div>
+  );
+}
---
+++ client/src/components/QuestionsAnswers/MoreAnsweredQ.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { useState } from 'react';
+//
+export default function MoreAnsweredQ() {
+  return (
+    <div>
+      <button type="submit">More Answered Questions make a state: true or false</button>
+    </div>
+  );
+}
