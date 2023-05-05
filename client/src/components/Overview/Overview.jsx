c196e64364e9a699c42f9190c33c27f929f59a7c
1998rtran <1998rtran@gmail.com>
2023-05-05 12:21:21 -0700

    initial API get request working

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ProductInfo from './components/ProductInfo';
 import StyleSelector from './components/StyleSelector';
 import AddToCart from './components/AddToCart';
 import ImageGallery from './components/ImageGallery';
 import Description from './components/Description';
-
+const axios = require('axios');
 
 export default function Overview() {
+  const [overview, setOverview] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/overview/products')
+      .then((response) => {
+        console.log(response);
+      });
+  }, [])
+
   return (
     <div className="overview">
       <ImageGallery />
--- server/overview.js
+++ server/overview.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
 const express = require('express');
-
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/about', (req, res) => {
-  res.send('About birds');
+const auth = process.env.TOKEN;
+
+router.get('/products', (req, res) => {
+  console.log('This is the request body: ', req.body);
+  let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/`
+  let options = {
+    headers: {
+      'Authorization': auth
+    }
+  }
+  axios.get(url, options)
+    .then((response)=>{
+      console.log('This is the response: ', response.data);
+      res.json(response.data);
+    })
+    .catch((err)=>{
+      console.log('server error: ', err.response.data)
+      res.status(500).send('Error fetching data from API');
+    })
 });
 
 module.exports = router;
