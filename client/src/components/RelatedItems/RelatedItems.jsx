6d3136d23e2285ce72c5fd4abbb7426240c656db
Keith Foreman <keithforeman218@gmail.com>
2023-05-05 16:02:45 -0700

    Add productID prop to RelatedItems. Add server request to pull data.

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import RelatedProductsList from './RelatedProductsList';
 import YourOutfitList from './YourOutfitList';
 import Comparison from './Comparison';
-
 import styles from './Styles.jsx';
-const { ListContainer } = styles;
 
-export default function RelatedItems() {
+const { ListContainer } = styles;
+const { useState, useEffect } = React;
+
+export default function RelatedItems( {productID} ) {
+  const [items, setItems] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get(`/related/${productID}`, {
+  //     headers: {
+  //       Authorization: process.env.AUTH
+  //     }
+  //   })
+  //     .then((response) => {
+  //       setItems(response.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log('ERROR:', err);
+  //     });
+  // },[productID]);
+
   return (
     <div id="related-items-component">
     <ListContainer>
-      <RelatedProductsList />
+      <RelatedProductsList items={items}/>
 
       {/* adding just to test */}
       <Comparison />
