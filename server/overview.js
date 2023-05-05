8e478a18a4c6e5357f3bc51d49121ba816f21c53
1998rtran <1998rtran@gmail.com>
2023-05-05 16:00:58 -0700

    completed server side get requests

--- server/overview.js
+++ server/overview.js
@@ -15,7 +15,7 @@ router.get('/products', (req, res) => {
   }
   axios.get(url, options)
     .then((response)=>{
-      console.log('This is the response: ', response.data);
+      console.log('These are the products: ', response.data);
       res.json(response.data);
     })
     .catch((err)=>{
@@ -23,5 +23,59 @@ router.get('/products', (req, res) => {
       res.status(500).send('Error fetching data from API');
     })
 });
+
+router.get('/products/:product_id', (req, res) => {
+  let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.product_id}/`
+  let options = {
+    headers: {
+      'Authorization': auth
+    }
+  }
+  axios.get(url, options)
+    .then((response)=>{
+      console.log('This is the product data: ', response.data);
+      res.json(response.data);
+    })
+    .catch((err)=>{
+      console.log('server error: ', err.response.data)
+      res.status(500).send('Error fetching data from API');
+    })
+})
+
+router.get('/products/:product_id/styles', (req, res) => {
+  let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.product_id}/styles`
+  let options = {
+    headers: {
+      'Authorization': auth
+    }
+  }
+  axios.get(url, options)
+    .then((response)=>{
+      console.log("These are the product's styles: ", response.data);
+      res.json(response.data);
+    })
+    .catch((err)=>{
+      console.log('server error: ', err.response.data)
+      res.status(500).send('Error fetching data from API');
+    })
+})
+
+router.get('/products/:product_id/related', (req, res) => {
+  let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.product_id}/related`
+  let options = {
+    headers: {
+      'Authorization': auth
+    }
+  }
+  axios.get(url, options)
+    .then((response)=>{
+      console.log('This is the response: ', response.data);
+      res.json(response.data);
+    })
+    .catch((err)=>{
+      console.log('server error: ', err.response.data)
+      res.status(500).send('Error fetching data from API');
+    })
+})
 
 module.exports = router;
