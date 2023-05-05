a6db2ad00285b4f50bafebb0e89bcffac39b3063
1998rtran <1998rtran@gmail.com>
2023-05-05 10:59:50 -0700

    completed skeletal structure for Overview, added css to mimic ideal display

--- client/src/components/Overview/components/AddToCart.jsx
+++ client/src/components/Overview/components/AddToCart.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import QuantitySelector from './addtocart/QuantitySelector';
+import SizeSelector from './addtocart/SizeSelector';
+import Add from './addtocart/Add';
+import Favorite from './addtocart/Favorite';
 
 export default function AddToCart() {
   return (
     <div className="addtocart">
-      Add to cart box
+      <form onSubmit={(e)=>{e.preventDefault()}}>
+        <SizeSelector />
+        <QuantitySelector />
+        <Add />
+        <Favorite />
+      </form>
     </div>
   );
 }
--- client/src/components/Overview/components/StyleSelector.jsx
+++ client/src/components/Overview/components/StyleSelector.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function StyleSelector() {
+  // selected style might be from API
   return (
     <div className="styleselector">
-      Style selector box
+      <span><strong>Style &#8594;</strong>Selected Style</span>
     </div>
   );
 }
