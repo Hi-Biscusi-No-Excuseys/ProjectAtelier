83a5ffdaf6959d63e243483b84824c5671a98134
Kodi <30986906+kodiwalker@users.noreply.github.com>
2023-05-05 13:31:04 -0600

    Implemented and tested all Reviews API calls server-side

--- server/ratingsreviews.js
+++ server/ratingsreviews.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 router.get('/list', (req, res) => {
-  // req.body should look like:
+  // clients req.body should look like:
   // {
   //   page: 1,
   //   count: 2,
@@ -19,12 +19,96 @@ router.get('/list', (req, res) => {
       { Authorization: process.env.AUTH }
   })
     .then((response) => {
-      res.send(response.data);
+      res.json(response.data);
     })
     .catch((err) => {
-      console.error('API call failed:', err);
-      res.send(err);
+      console.error('Failed to get reviews:', err.response.data);
+      res.send(err.response.status);
     })
 });
+
+
+router.get('/meta', (req, res) => {
+  // clients req.body should look like:
+  // {
+  //   product_id: 40344 
+  // }
+  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/meta?product_id=${req.body.product_id}`;
+
+  axios.get(url, {
+    headers:
+      { Authorization: process.env.AUTH }
+  })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.error('Failed to get metadata:', err.response.data);
+      res.send(err.response.status);
+    })
+});
+
+
+router.post('/', (req, res) => {
+  // clients req.body should look like:
+  // {
+  //   product_id: 40344,
+  //   rating: 5,
+  //   summary: 'Perfect for the beach',
+  //   body: 'Style and quality is top notch. Will be buying more styles <3.',
+  //   recommend: true,
+  //   name: 'Kode',
+  //   email: 'required@m.com',
+  //   photos: ['url', 'url'],
+  //   characteristics: {135400: 4, 135403: 5}
+  // }
+  const config = {
+    headers: { Authorization: process.env.AUTH }
+  };
+  axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews', req.body, config)
+    .then((response) => {
+      res.sendStatus(response.status);
+    })
+    .catch((err) => {
+      console.error('Failed to post review:', err.response.data);
+      res.sendStatus(err.response.status);
+    })
+});
+
+
+router.put('/helpful', (req, res) => {
+  // clients req.body should look like:
+  // {
+  //   review_id: 40344 
+  // }
+
+  axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/${req.body.review_id}/helpful`, {}, { headers: { Authorization: process.env.AUTH } })
+    .then((response) => {
+      res.sendStatus(response.status);
+    })
+    .catch((err) => {
+      console.error('Failed to mark helpful:', err.response.data);
+      res.sendStatus(err.response.status);
+    })
+});
+
+
+router.put('/report', (req, res) => {
+  // clients req.body should look like:
+  // {
+  //   review_id: 40344 
+  // }
+
+  axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/${req.body.review_id}/report`, {}, { headers: { Authorization: process.env.AUTH } })
+    .then((response) => {
+      res.sendStatus(response.status);
+    })
+    .catch((err) => {
+      console.error('Failed to report:', err.response.data);
+      res.sendStatus(err.response.status);
+    })
+});
+
+
 
 module.exports = router;
