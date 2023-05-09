fcb038e300757a9ee36e51ac9ad97a50b7b6a1ae
Kodi <30986906+kodiwalker@users.noreply.github.com>
2023-05-09 16:15:46 -0600

    Implemented show more/less for long review bodies, and photo thumbnails and modals for review tile

--- client/src/components/RatingsReviews/components/ReviewTile.jsx
+++ client/src/components/RatingsReviews/components/ReviewTile.jsx
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
+import { createPortal } from 'react-dom';
 import SolidStars from './SolidStars.jsx';
 
 export default function ReviewTile({review}) {
   const [showMore, setShowMore] = useState(false);
+  const [showPhotoModal, setShowPhotoModal] = useState(false);
+  const [photoURL, setPhotoURL] = useState('');
+
   const date = new Date(review.date);
   const formattedDate = date.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
 
+  function handlePhotoModal(url) {
+    setPhotoURL(url);
+    setShowPhotoModal(true);
+  };
 
   return (
     <div className='review-tile'>
@@ -14,13 +22,28 @@ export default function ReviewTile({review}) {
         <span>{review.reviewer_name}, {formattedDate}</span>
       </div>
 
-      <p className='summary'>{review.summary}</p>
-      <p>
+      <p className='review-summary'>{review.summary}</p>
+      <p className='review-body'>
         {showMore ? review.body : review.body.substring(0, 250)}
         {review.body.length > 250 ? 
         <button onClick={() => setShowMore(!showMore)}>{showMore ? 'Show less' : 'Show more'}</button>
         : null}
       </p>
+
+      {review.photos.length ? 
+        <div className='review-photos'>
+          {review.photos.map((photo) => {
+            return <img className="review-photo-thumb" src={photo.url} key={photo.id} onClick={() => handlePhotoModal(photo.url)}/>
+          })}
+        </div>
+      : null}
+
+      {showPhotoModal && createPortal(
+      <div id='review-photo-modal'>
+        <img id='review-photo' src={photoURL}></img>
+        <button onClick={() => setShowPhotoModal(false)}>Close</button>
+      </div>
+      , document.body)}
 
       {review.recommend ? <div className='recommend'>✓ I recommend this product</div> : null}
       
