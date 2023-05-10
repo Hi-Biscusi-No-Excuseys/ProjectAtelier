a6e74cf3751ce6302d42570d3aa1cd102c4f4b5b
Keith Foreman <keithforeman218@gmail.com>
2023-05-10 12:15:51 -0700

    Refactoring API requests, rearranged where logic sits.

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -14,62 +14,85 @@ export default function RelatedItems( {product, setProduct} ) {
 
 // move the sessionStorage (and/or change to localStorage) into the YourOutfit component. Not sure it's really needed here. But it was useful during development to avoid hitting the API so many times.
   useEffect(() => {
-    if (sessionStorage.getItem('RelatedItems') && items.length === 0) {
-      // console.log('Snagging from sessionStorage:');
-      setItems(JSON.parse(sessionStorage.getItem('RelatedItems')));
-    } else {
+    // if (sessionStorage.getItem('RelatedItems') && items.length === 0) {
+    //   // console.log('Snagging from sessionStorage:');
+    //   setItems(JSON.parse(sessionStorage.getItem('RelatedItems')));
+    // } else {
       axios.get(`/relateditems/related/${product.id}`)
         .then((firstResponse) => {
-          // console.log('What did we get? : ', firstResponse.data);
-
-          const promises = [];
+          console.log('What did we get? : ', firstResponse.data);
+          let url = '';
+          const promiseProductLevel = [];
 
           for (let i = 0; i < firstResponse.data.length; i++) {
-            let relatedID = firstResponse.data[i].id;
-            let url = `/overview/products/${relatedID}/styles`;
-            // console.log('URL #', i, url);
-            promises.push(axios.get(url));
+            // url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${firstResponse.data[i]}`;
+            url = `/overview/products/${firstResponse.data[i]}`;
+            console.log('URL #', i, url);
+            promiseProductLevel.push(axios.get(url));
           }
 
-          axios.all(promises)
+          axios.all(promiseProductLevel)
             .then((secondResponse) => {
-              // console.log('What styles did we get? : ', secondResponse);
-              const styledItems = [];
+              const productInfo = [];
 
-              for (let i = 0; i < firstResponse.data.length; i++) {
-                  // console.log('Ending up with: ', Object.assign(firstResponse.data[i], secondResponse[i].data));
-                  if (styledItems.length === 0) {
-                    styledItems.push(Object.assign(firstResponse.data[i], secondResponse[i].data));
-                  } else {
-                    let found = false;
-                    for (let j = 0; j < styledItems.length; j++) {
-                      if (styledItems[j].id === firstResponse.data[i].id) {
-                        found = true;
-                        // console.log('>>>>> Found duplicate, do not save.');
-                        break;
-                      }
-                    }
-
-                    if (!found) {
-                      styledItems.push(Object.assign(firstResponse.data[i], secondResponse[i].data));
-                    }
-                  }
-
+              for (let i = 0; i < secondResponse.length; i++) {
+                productInfo.push(secondResponse[i].data);
               }
 
-              setItems(styledItems);
-              sessionStorage.setItem('RelatedItems', JSON.stringify(styledItems));
-              // console.log('We set something : ', JSON.parse(sessionStorage.getItem('RelatedItems')));
+
+              // begin refactor here
+              const promiseStyleLevel = [];
+
+              for (let i = 0; i < productInfo.length; i++) {
+                let relatedID = productInfo[i].id;
+                // url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${relatedID}/styles`;
+                url = `/overview/products/${relatedID}/styles`;
+                console.log('URL #', i, url);
+                promiseStyleLevel.push(axios.get(url));
+              }
+
+              axios.all(promiseStyleLevel)
+                .then((thirdResponse) => {
+                  // console.log('What styles did we get? : ', thirdResponse);
+                  const styledItems = [];
+
+                  for (let i = 0; i < productInfo.length; i++) {
+                      // console.log('Ending up with: ', Object.assign(productInfo[i], thirdResponse[i].data));
+                      if (styledItems.length === 0) {
+                        styledItems.push(Object.assign(productInfo[i], thirdResponse[i].data));
+                      } else {
+                        let found = false;
+                        for (let j = 0; j < styledItems.length; j++) {
+                          if (styledItems[j].id === productInfo[i].id) {
+                            found = true;
+                            // console.log('>>>>> Found duplicate, do not save.');
+                            break;
+                          }
+                        }
+
+                        if (!found) {
+                          styledItems.push(Object.assign(productInfo[i], thirdResponse[i].data));
+                        }
+                      }
+                  }
+
+                  setItems(styledItems);
+                  console.log('>>>>>>>>>>>>>>> NEW DATA: ', styledItems);
+                  // sessionStorage.setItem('RelatedItems', JSON.stringify(styledItems));
+                  // console.log('We set something : ', JSON.parse(sessionStorage.getItem('RelatedItems')));
+                  // res.status(200).send(styledItems);
+                })
+                .catch((err) => {
+                  console.log('Error retrieving styles.', err);
+                });
             })
             .catch((err) => {
-              console.log('Error retrieving style.', err);
-              throw (err);
-            })
+              console.log('Error retrieving multiple products info from API.', err);
+            });
         })
         .catch((err) => {
-          console.log('ERROR:', err);
+          console.log('ERROR with new GET request:', err);
         });
-    }
   },[product]);
 
   const addToOutfit = (item) => {
