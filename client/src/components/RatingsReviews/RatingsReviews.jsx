3d5433af88f4babd6529e5540d1a6eb0bd8ed1bf
Kodi <30986906+kodiwalker@users.noreply.github.com>
2023-05-12 16:41:50 -0600

    Fixed all eslint errors

--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import RatingBreakdown from './components/RatingBreakdown.jsx';
-import ProductBreakdown from './components/ProductBreakdown.jsx';
-import SortOptions from './components/SortOptions.jsx';
-import ReviewsList from './components/ReviewsList.jsx';
+/* eslint-disable no-restricted-syntax */
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import RatingBreakdown from './components/RatingBreakdown';
+import ProductBreakdown from './components/ProductBreakdown';
+import SortOptions from './components/SortOptions';
+import ReviewsList from './components/ReviewsList';
 
-export default function RatingsReviews({product}) {
+export default function RatingsReviews({ product }) {
   // product.id = 40432;
   const [sort, setSort] = useState('relevant');
   const [amount, setAmount] = useState(null);
@@ -13,71 +14,85 @@ export default function RatingsReviews({product}) {
   const [avg, setAvg] = useState(null);
   const [reviews, setReviews] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [starFilter, setStarFilter] = useState({1: false, 2: false, 3: false, 4: false, 5: false});
-
-
+  const [starFilter, setStarFilter] = useState({
+    1: false, 2: false, 3: false, 4: false, 5: false,
+  });
 
   useEffect(() => {
-    axios.get('/reviews/meta', { params: {product_id: product.id} })
-    .then((response) => {
-      //^ Sets average review rating to nearest 10th decimal place
-      const ratings = response.data.ratings;
-      let numOfReviews = 0;
-      let numOfStars = 0;
-      for (const key in ratings) {
-        numOfStars = numOfStars + (key * ratings[key]);
-        numOfReviews = numOfReviews + parseInt(ratings[key], 10);
-      }
-      const longAvg = numOfStars / numOfReviews;
-      setAvg(Math.round(longAvg * 10) / 10);
+    axios.get('/reviews/meta', { params: { product_id: product.id } })
+      .then((response) => {
+      // ^ Sets average review rating to nearest 10th decimal place
+        const { ratings } = response.data;
+        let numOfReviews = 0;
+        let numOfStars = 0;
+        for (const key in ratings) {
+          if (ratings[key]) {
+            numOfReviews += parseInt(ratings[key], 10);
+            numOfStars += (key * ratings[key]);
+          }
+        }
+        const longAvg = numOfStars / numOfReviews;
+        setAvg(Math.round(longAvg * 10) / 10);
 
-      //^ Sets number of reviews
-      setAmount(parseInt(response.data.recommended.false, 10) + parseInt(response.data.recommended.true, 10));
+        // ^ Sets number of reviews
+        setAmount(parseInt(response.data.recommended.false, 10)
+        + parseInt(response.data.recommended.true, 10));
 
-      //^ Sets meta data
-      setMetaData(response.data);
+        // ^ Sets meta data
+        setMetaData(response.data);
 
-      return axios.get('/reviews/list', { params: {
-        page: 1,
-        count: parseInt(response.data.recommended.false, 10) + parseInt(response.data.recommended.true, 10),
-        sort: sort,
-        product_id: product.id
-      }})
-    })
-    .then((response) => {
-      setReviews(response.data.results);
-      setLoading(false);
-    })
-    .catch((err) => {
-      console.error('Client failed to get reviews:', err);
-      setLoading(false);
-    })
-  }, [product, sort]) //TODO: Add new review posted and star filter dependencies
-
-
+        return axios.get('/reviews/list', {
+          params: {
+            page: 1,
+            count: parseInt(response.data.recommended.false, 10)
+            + parseInt(response.data.recommended.true, 10),
+            sort,
+            product_id: product.id,
+          },
+        });
+      })
+      .then((response) => {
+        setReviews(response.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error('Client failed to get reviews:', err);
+        setLoading(false);
+      });
+  }, [product, sort]); // TODO: Add new review posted and star filter dependencies
 
   return (
-    !loading ?
-    <div id='reviews'>
-      <h3>Ratings & Reviews</h3>
+    !loading
+      ? (
+        <div id="reviews">
+          <h3>Ratings & Reviews</h3>
 
-      <div id='breakdown-list'>
-        <aside>
-          <RatingBreakdown product={product.id} amount={amount} metaData={metaData} avg={avg} setStarFilter={setStarFilter} starFilter={starFilter}/>
-          <ProductBreakdown characteristics={metaData.characteristics}/>
-        </aside>
+          <div id="breakdown-list">
+            <aside>
+              <RatingBreakdown
+                product={product.id}
+                amount={amount}
+                metaData={metaData}
+                avg={avg}
+                setStarFilter={setStarFilter}
+                starFilter={starFilter}
+              />
+              <ProductBreakdown characteristics={metaData.characteristics} />
+            </aside>
 
-        <div id='sort-and-list'>
-          <SortOptions sort={sort} setSort={setSort} amount={amount} />
-          <ReviewsList reviews={reviews} starFilter={starFilter} />
+            <div id="sort-and-list">
+              <SortOptions sort={sort} setSort={setSort} amount={amount} />
+              <ReviewsList reviews={reviews} starFilter={starFilter} />
+            </div>
+          </div>
+
         </div>
-      </div>
-
-    </div>
-    : 
-    <div id='loading-container'>
-    <h3>Ratings & Reviews</h3>
-    <p id='loading'>Loading...</p>
-    </div>
+      )
+      : (
+        <div id="loading-container">
+          <h3>Ratings & Reviews</h3>
+          <p id="loading">Loading...</p>
+        </div>
+      )
   );
 }
