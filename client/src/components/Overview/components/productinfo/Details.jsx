3d6bd56c87442b64d51cca4c6b230fd44b41c03b
1998rtran <1998rtran@gmail.com>
2023-05-18 17:32:25 -0700

    completed implementation of share buttons

--- client/src/components/Overview/components/productinfo/Details.jsx
+++ client/src/components/Overview/components/productinfo/Details.jsx
@@ -41,6 +41,22 @@ export default function Details({
           </strike>
         </p>
       )}
+      <div className="socialMediaContainer">
+        <a data-pin-do="buttonBookmark" data-pin-tall="true" href="https://www.pinterest.com/pin/create/button/">Save</a>
+        <div className="fb-share-button" data-href="http://13.57.236.58:3000/" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2F13.57.236.58%3A3000%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore" rel="noreferrer">Share</a></div>
+        <a
+          className="twitter-share-button"
+          href="https://twitter.com/intent/tweet"
+          data-size="large"
+          data-text="custom share text"
+          data-url="https://dev.twitter.com/web/tweet-button"
+          data-hashtags="example,demo"
+          data-via="twitterdev"
+          data-related="twitterapi,twitter"
+        >
+          Tweet
+        </a>
+      </div>
     </div>
   );
 }
